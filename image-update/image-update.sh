cc3ca5dd72c277dad752998852f45199638c4dfe
obabec <ond.babec@gmail.com>
2021-08-03 13:04:21 +0200

    Sync crd (#23)
    * Sync deployment to target deployment repository

--- image-update/image-update.sh
+++ image-update/image-update.sh
@@ -25,7 +25,14 @@ elif [[ -z "${CURRENT_DEPLOYMENT_REPO}" ]]; then
 elif [[ -z "${TARGET_ORG_REPO}" ]]; then
     echo "Missing target organization link"
     exit 3
+elif [[ -z ${SYNC_CRD_REPO} ]]; then 
+    echo "Missing target CRD sync repo"
+    exit 4
+elif [[ -z ${SYNC_CRD_PATH} ]]; then 
+    echo "Missing target CRD path"
+    exit 5
 fi
+
 
 TARGET_DIR="${PWD}/../current_deployment"
 echo $TARGET_DIR
@@ -36,6 +43,18 @@ git config --global user.name "$GITHUB_USERNAME"
 echo "Cloning repository"
 echo "================================================"
 git clone $CURRENT_DEPLOYMENT_REPO $TARGET_DIR
+
+echo "================================================"
+echo "Cloning target CRD repo for sync"
+
+SYNC_CRD_DIR="${PWD}/../sync_repo"
+git clone $SYNC_CRD_REPO $SYNC_CRD_DIR
+cp -r $SYNC_CRD_DIR/$SYNC_CRD_PATH/* $TARGET_DIR/$YAML_BUNDLE_PATH/
+
+echo "================================================"
+echo "Moving into synced deployment repository"
+echo "================================================"
+
 pushd $TARGET_DIR
 CURRENT_DEPLOYMENT_REPO=$(echo $CURRENT_DEPLOYMENT_REPO | cut -d '/' -f3-)
 
@@ -43,6 +62,21 @@ git remote set-url origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$CURRENT_DEPLO
 git checkout -b $BRANCH
 
 FILE_NAME=$(ls $YAML_BUNDLE_PATH | $GREP Deployment)
+
+# Change floating tags to random digest
+IMAGES_TAGS=$(cat $YAML_BUNDLE_PATH/$FILE_NAME | grep $TARGET_ORG_REPO/ | sort -u | awk '{$1=$1};1' | cut -d ' ' -f2- | cut -d '/' -f3- | sort -u | tr '\n' ';')
+echo $IMAGES_TAGS
+IFS=';' read -r -a IMGS <<< $IMAGES_TAGS
+T=11111
+for ELEMENT_O in "${IMGS[@]}"
+do
+    CURRENT_TAG=$(echo $ELEMENT_O | cut -d ':' -f2)
+    CURRENT_TAG=$(echo ":$CURRENT_TAG")
+    IMAGE_NAME=$(echo $ELEMENT_O | cut -d ':' -f1)
+    NEW_DIGEST="$IMAGE_NAME@sha:$T"
+    T=$((T+1))
+    $SED -i 's#'"$IMAGE_NAME$CURRENT_TAG"'#'"$NEW_DIGEST"'#g' $YAML_BUNDLE_PATH/$FILE_NAME
+done
 
 IMAGES_PLAIN=$(cat $YAML_BUNDLE_PATH/$FILE_NAME | $GREP $TARGET_ORG_REPO/ | sort -u | awk '{$1=$1};1' | cut -d ' ' -f2-| tr '\n' ';')
 IFS=';' read -r -a IMAGES <<< $IMAGES_PLAIN
@@ -73,12 +107,13 @@ done
 
 echo "================================================"
 echo "Adding changes to repository"
-git add $YAML_BUNDLE_PATH/$FILE_NAME
+git add $YAML_BUNDLE_PATH/*
 git commit -m "Image update: $($DATE "+%Y-%m-%d %T")"
 git push origin $BRANCH
 popd
 echo "================================================"
 echo "Cleaning"
 rm -rf $TARGET_DIR
+rm -rf $SYNC_CRD_DIR
 echo "================================================"
 exit 0
