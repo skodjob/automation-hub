8106b2aa7f2aa616553f2839ab90de6bc14ea6e7
David Kornel <kornys@outlook.com>
2022-01-12 13:28:35 +0100

    fix

--- scripts/copy-secrets.sh
+++ scripts/copy-secrets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 oc login -u $OC_USERNAME -p $OC_PASSWORD $OC_API_URL --insecure-skip-tls-verify=true
-oc get secret -n strimzi-kafka her-ur -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences)' - | oc apply -n strimzi-twitter-connector -f -
-oc get secret -n strimzi-kafka anubis-cluster-ca-cert -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences)' - | oc apply -n strimzi-twitter-connector -f -
-oc get secret -n strimzi-kafka hathor -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences)' - | oc apply -n strimzi-twitter-connector -f -
+oc get secret -n strimzi-kafka her-ur -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences, .metadata.annotations)' - | oc apply -n strimzi-twitter-connector -f -
+oc get secret -n strimzi-kafka anubis-cluster-ca-cert -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences, .metadata.annotations)' - | oc apply -n strimzi-twitter-connector -f -
+oc get secret -n strimzi-kafka hathor -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences, .metadata.annotations)' - | oc apply -n strimzi-twitter-connector -f -
