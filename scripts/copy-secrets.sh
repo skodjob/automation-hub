d6446999206dd451e3ba99d83711db670331fe91
David Kornel <kornys@outlook.com>
2022-01-13 15:19:19 +0100

    Fix sync secrets

--- scripts/copy-secrets.sh
+++ scripts/copy-secrets.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-touch /tmp/kubeconfig
-KUBECONFIG=/tmp/kubeconfig oc login -u $OC_USERNAME -p $OC_PASSWORD $OC_API_URL --insecure-skip-tls-verify=true
-KUBECONFIG=/tmp/kubeconfig oc get secret -n strimzi-kafka her-ur -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences,.metadata.annotations,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid)' - | oc apply -n strimzi-twitter-connector -f -
-KUBECONFIG=/tmp/kubeconfig oc get secret -n strimzi-kafka anubis-cluster-ca-cert -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences,.metadata.annotations,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid)' - | oc apply -n strimzi-twitter-connector -f -
-KUBECONFIG=/tmp/kubeconfig oc get secret -n strimzi-kafka hathor -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences,.metadata.annotations,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid)' - | oc apply -n strimzi-twitter-connector -f -
+oc login -u $OC_USERNAME -p $OC_PASSWORD $OC_API_URL --insecure-skip-tls-verify=true
+oc get secret -n strimzi-kafka her-ur -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences,.metadata.annotations,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid)' - | oc apply -n strimzi-twitter-connector -f -
+oc get secret -n strimzi-kafka anubis-cluster-ca-cert -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences,.metadata.annotations,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid)' - | oc apply -n strimzi-twitter-connector -f -
+oc get secret -n strimzi-kafka hathor -o yaml | yq eval 'del(.metadata.namespace,.metadata.ownerReferences,.metadata.annotations,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid)' - | oc apply -n strimzi-twitter-connector -f -
