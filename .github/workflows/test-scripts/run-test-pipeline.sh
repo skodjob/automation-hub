b44384f4c444ddd507e462c9382dc6cff31becb3
David Kornel <kornys@outlook.com>
2022-09-05 20:41:23 +0200

    Add ansible-navigator exec configuration + simplify structure

--- .github/workflows/test-scripts/run-test-pipeline.sh
+++ .github/workflows/test-scripts/run-test-pipeline.sh
@@ -5,6 +5,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 REPO_ROOT="${DIR}/../../.."
 
 kubectl create ns test
-kubectl create -f "${REPO_ROOT}/tealc/files/tekton/pipelines/test/" -n test
+kubectl create -f "${REPO_ROOT}/install/files/tekton/pipelines/test/" -n test
 kubectl wait pipelinerun pipeline-run-test -n test --for condition=Succeeded --timeout 120s
 kubectl delete ns test
