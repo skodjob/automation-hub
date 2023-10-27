658d454ebcf31f384b37147b7c1ab925240d58aa
Jakub Stejskal <xstejs24@gmail.com>
2023-10-27 09:44:02 +0200

    Provision clsuters through Hive (#257)
    * WIP for HIVE integration
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Add cluster provisioning through Hive
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Comment out used code
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Fix lint
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Fix lint v2
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Fix lint v3
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Fix lint v4
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    ---------
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- scripts/copy-secrets.sh
+++ scripts/copy-secrets.sh
@@ -5,12 +5,8 @@ set -x
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${DIR}/common.sh"
 
-if [ -z "$SOURCE_USER" ] ; then
+if [ -z "$SOURCE_TOKEN" ] ; then
         err 'Missing SOURCE_USER env var' >&2
-        exit 1
-fi
-if [ -z "$SOURCE_PASS" ] ; then
-        err 'Missing SOURCE_PASS env var' >&2
         exit 1
 fi
 if [ -z "$SOURCE_URL" ] ; then
@@ -18,12 +14,8 @@ if [ -z "$SOURCE_URL" ] ; then
         exit 1
 fi
 
-if [ -z "$TARGET_USER" ] ; then
+if [ -z "$TARGET_TOKEN" ] ; then
         err 'Missing TARGET_USER env var' >&2
-        exit 1
-fi
-if [ -z "$TARGET_PASS" ] ; then
-        err 'Missing TARGET_PASS env var' >&2
         exit 1
 fi
 if [ -z "$TARGET_URL" ] ; then
@@ -46,11 +38,11 @@ fi
 OUTPUT_NAME=/tmp/secrets.yaml
 
 info "Logging to source cluster and getting secret"
-oc login -u "${SOURCE_USER}" -p "${SOURCE_PASS}" "--insecure-skip-tls-verify=true" "${SOURCE_URL}"
+oc login --token "${SOURCE_TOKEN}" "--insecure-skip-tls-verify=true" "${SOURCE_URL}"
 oc get secret -l "${LABEL_SELECTOR}" -o yaml -n "${SOURCE_NAMESPACE}" | yq eval "del(.items[].metadata.namespace,.items[].metadata.ownerReferences,.items[].metadata.annotations,.items[].metadata.managedFields,.items[].metadata.resourceVersion,.items[].metadata.uid)" - > $OUTPUT_NAME
 
 cat "${OUTPUT_NAME}"
 
 info "Logging to target cluster and applying secret"
-oc login -u "${TARGET_USER}" -p "${TARGET_PASS}" "--insecure-skip-tls-verify=true" "${TARGET_URL}"
+oc login --token "${TARGET_TOKEN}" "--insecure-skip-tls-verify=true" "${TARGET_URL}"
 oc apply -f "${OUTPUT_NAME}" -n "${TARGET_NAMESPACE}"
